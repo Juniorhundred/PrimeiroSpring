7e3e8cffad059485fb6e90b6f41a7d5d3a3984d6
SandroJunior <juniorfuca2012@gmail.com>
2021-11-26 16:06:22 -0300

    Primeiro Commit
    Primeiros arquivos commitados

---
+++ src/main/java/br/com/primeiroprojetospring/PrimeiroProjetoSpringImplApplication.java
@@ -0,0 +1,13 @@
+package br.com.primeiroprojetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringImplApplication.class, args);
+	}
+
+}
